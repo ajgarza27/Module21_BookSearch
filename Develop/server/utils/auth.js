fd4e121f45dc361735ce826c5ef2160dc2df58be
unknown <ajgarza27@gmail.com>
2024-08-15 20:42:10 -0500

    fixing issues

--- Develop/server/utils/auth.js
+++ Develop/server/utils/auth.js
@@ -1,39 +1,26 @@
 const jwt = require('jsonwebtoken');
 
-// set token secret and expiration date
-const secret = 'mysecretsshhhhh';
+const secret = 'your_secret_key';
 const expiration = '2h';
 
-module.exports = {
-  // function for our authenticated routes
-  authMiddleware: function (req, res, next) {
-    // allows token to be sent via  req.query or headers
-    let token = req.query.token || req.headers.authorization;
+const authMiddleware = ({ req }) => {
+  let token = req.headers.authorization || '';
 
-    // ["Bearer", "<tokenvalue>"]
-    if (req.headers.authorization) {
-      token = token.split(' ').pop().trim();
-    }
-
-    if (!token) {
-      return res.status(400).json({ message: 'You have no token!' });
-    }
-
-    // verify token and get user data out of it
+  if (token) {
     try {
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
       req.user = data;
     } catch {
       console.log('Invalid token');
-      return res.status(400).json({ message: 'invalid token!' });
     }
+  }
 
-    // send to next endpoint
-    next();
-  },
-  signToken: function ({ username, email, _id }) {
-    const payload = { username, email, _id };
-
-    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
-  },
+  return req;
 };
+
+const signToken = ({ email, _id, username }) => {
+  const payload = { email, _id, username };
+  return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+};
+
+module.exports = { authMiddleware, signToken };
--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# Book Search Engine Starter Code
+# Book Search Engine
+
+## Description
+
+The Book Search Engine is a MERN stack application that allows users to search for books using the Google Books API. The application is refactored to use GraphQL with Apollo Server to handle queries and mutations. Users can sign up, log in, search for books, save their favorite books, and view their saved books.
+
+## Features
+
+- Search for Books: Search for books using keywords and view detailed results including title, author, description, image, and a link to Google Books.
+- User Authentication: Users can sign up, log in, and manage their accounts.
+- ave Books: Logged-in users can save books to their account and view their saved books.
+- Remove Books: Users can remove books from their saved list.
+
+## Technologies Used
+
+- Frontend: React, Vite
+- Backend: Node.js, Express.js, Apollo Server, GraphQL
+- Database: MongoDB
+- Deployment: Render
+
+## Getting Started
+
+### Prerequisites
+
+- Node.js
+- npm (Node Package Manager)
+
+### Installation
+
+1. **Clone the repository:**
+
+   ```bash
+   git clone https://github.com/ajgarza27/Module21_BookSearch.git
+   cd Module21_BookSearch
+
+   
+
